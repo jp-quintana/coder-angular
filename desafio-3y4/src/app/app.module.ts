ea1f02c696ae528b262e58da6381a192248eeed3
jp-quintana <juanquintana1996@gmail.com>
2023-09-28 02:22:36 -0300

    Add Desafio 3 y 4

--- desafio-3y4/src/app/app.module.ts
+++ desafio-3y4/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
+import { RoundListDirective } from './directives/round-list.directive';
 
 @NgModule({
-  declarations: [AppComponent, ListComponent],
+  declarations: [AppComponent, ListComponent, RoundListDirective],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
